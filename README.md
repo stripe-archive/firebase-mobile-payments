66171f10f028bb0cbc822c383f3bb8e6408f69fa
Thorsten Schaeff <thorsten@stripe.com>
2020-07-15 12:59:57 +0800

    Add functions.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Firebase Mobile Payments
+
+## Setup
+
+- Create a Firebase Project using the [Firebase Developer Console](https://console.firebase.google.com)
+- Enable billing on your project by switching to the Blaze or Flame plan. See [pricing](https://firebase.google.com/pricing/) for more details. This is required to be able to do requests to non-Google services.
+- Enable Google & Email sign-in in your [authentication provider settings](https://console.firebase.google.com/project/_/authentication/providers).
+- Install [Firebase CLI Tools](https://github.com/firebase/firebase-tools) if you have not already and log in with `firebase login`.
+- Configure this sample to use your project using `firebase use --add` and select your project.
+- Install dependencies locally by running: `cd functions; npm install; cd -`
+- [Add your Stripe API Secret Key](https://dashboard.stripe.com/account/apikeys) to firebase config:
+  ```bash
+  firebase functions:config:set stripe.secret=<YOUR STRIPE SECRET KEY>
+  ```
+- Deploy your project using `cd functions; npm run deploy; cd -`
---
+++ functions/index.js
@@ -0,0 +1,179 @@
+/**
+ * Copyright 2020 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const { Logging } = require('@google-cloud/logging');
+const logging = new Logging({
+  projectId: process.env.GCLOUD_PROJECT,
+});
+const stripe = require('stripe')(functions.config().stripe.secret, {
+  apiVersion: '2020-03-02',
+});
+
+/**
+ * When a user is created, create a Stripe customer object for them.
+ */
+exports.createStripeCustomer = functions.auth.user().onCreate(async (user) => {
+  const customer = await stripe.customers.create({ email: user.email });
+
+  await admin.firestore().collection('stripe_customers').doc(user.uid).set({
+    customer_id: customer.id,
+  });
+  return;
+});
+
+/**
+ * Set up an ephemeral key.
+ *
+ * @see https://stripe.com/docs/mobile/android/basic#set-up-ephemeral-key
+ */
+exports.createEphemeralKey = functions.https.onCall(async (data, context) => {
+  // Checking that the user is authenticated.
+  if (!context.auth) {
+    // Throwing an HttpsError so that the client gets the error details.
+    throw new functions.https.HttpsError(
+      'failed-precondition',
+      'The function must be called while authenticated!'
+    );
+  }
+  const uid = context.auth.uid;
+  try {
+    if (!uid) throw new Error('Not authenticated!');
+    // Get stripe customer id
+    const customer = (
+      await admin.firestore().collection('stripe_customers').doc(uid).get()
+    ).data().stripeId;
+    const key = await stripe.ephemeralKeys.create(
+      { customer: customer.id },
+      { stripe_version: data.api_version }
+    );
+    return key;
+  } catch (error) {
+    throw new functions.https.HttpsError('internal', error.message);
+  }
+});
+
+/**
+ * When a payment document is written on the client,
+ * this function is triggered to create the PaymentIntent in Stripe.
+ *
+ * @see https://stripe.com/docs/mobile/android/basic#complete-the-payment
+ */
+exports.createStripePayment = functions.firestore
+  .document('stripe_customers/{userId}/payments/{pushId}')
+  .onCreate(async (snap, context) => {
+    const { amount, currency } = snap.data();
+    try {
+      // Look up the Stripe customer id.
+      const customer = (await snap.ref.parent.parent.get()).data().customer_id;
+      // Create a charge using the pushId as the idempotency key
+      // to protect against double charges.
+      const idempotencyKey = context.params.pushId;
+      const payment = await stripe.paymentIntents.create(
+        {
+          amount,
+          currency,
+          customer,
+        },
+        { idempotencyKey }
+      );
+      // If the result is successful, write it back to the database.
+      await snap.ref.set(payment);
+    } catch (error) {
+      // We want to capture errors and render them in a user-friendly way, while
+      // still logging an exception with StackDriver
+      console.log(error);
+      await snap.ref.set({ error: userFacingMessage(error) }, { merge: true });
+      await reportError(error, { user: context.params.userId });
+    }
+  });
+
+/**
+ * When a user deletes their account, clean up after them
+ */
+exports.cleanupUser = functions.auth.user().onDelete(async (user) => {
+  const dbRef = admin.firestore().collection('stripe_customers');
+  const customer = (await dbRef.doc(user.uid).get()).data();
+  await stripe.customers.del(customer.customer_id);
+  // Delete the customers payments & payment methods in firestore.
+  const snapshot = await dbRef
+    .doc(user.uid)
+    .collection('payment_methods')
+    .get();
+  snapshot.forEach((snap) => snap.ref.delete());
+  await dbRef.doc(user.uid).delete();
+  return;
+});
+
+// TODO webhook handler
+
+/**
+ * To keep on top of errors, we should raise a verbose error report with Stackdriver rather
+ * than simply relying on console.error. This will calculate users affected + send you email
+ * alerts, if you've opted into receiving them.
+ */
+
+// [START reporterror]
+
+function reportError(err, context = {}) {
+  // This is the name of the StackDriver log stream that will receive the log
+  // entry. This name can be any valid log stream name, but must contain "err"
+  // in order for the error to be picked up by StackDriver Error Reporting.
+  const logName = 'errors';
+  const log = logging.log(logName);
+
+  // https://cloud.google.com/logging/docs/api/ref_v2beta1/rest/v2beta1/MonitoredResource
+  const metadata = {
+    resource: {
+      type: 'cloud_function',
+      labels: { function_name: process.env.FUNCTION_NAME },
+    },
+  };
+
+  // https://cloud.google.com/error-reporting/reference/rest/v1beta1/ErrorEvent
+  const errorEvent = {
+    message: err.stack,
+    serviceContext: {
+      service: process.env.FUNCTION_NAME,
+      resourceType: 'cloud_function',
+    },
+    context: context,
+  };
+
+  // Write the error log entry
+  return new Promise((resolve, reject) => {
+    log.write(log.entry(metadata, errorEvent), (error) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve();
+    });
+  });
+}
+
+// [END reporterror]
+
+/**
+ * Sanitize the error message for the user.
+ */
+function userFacingMessage(error) {
+  return error.type
+    ? error.message
+    : 'An error occurred, developers have been alerted';
+}
