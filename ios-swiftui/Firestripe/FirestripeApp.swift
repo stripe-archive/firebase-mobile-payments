a723cf91a040a61d60b579ae2e55c172b32a100a
Thorsten Schaeff <thorsten@stripe.com>
2021-03-11 18:11:02 +0800

    feat: add ios swiftui project

---
+++ ios-swiftui/Firestripe/FirestripeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirestripeApp.swift
+//  Firestripe
+//
+//  Created by Thorsten Schaeff on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FirestripeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
